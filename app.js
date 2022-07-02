9b3763c17fcf97d5939de418987e1ba63d9c26a3
+ <+>
2022-07-02 11:15:12 +0300

    js select items

---
+++ app.js
@@ -0,0 +1,26 @@
+//select items
+const alert = document.querySelector('.alert');
+const forum = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
+
+//edit option
+
+
+
+//event listeners
+
+
+//functions
+
+
+
+//local storage
+
+
+
+
+//setup items
